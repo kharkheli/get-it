9298db8cebddac9794d5536bbd84a33a5a74568f
ilia kharkheli <iliakharkheli6@gmail.com>
2023-01-31 16:01:24 +0400

    correctly integrate express js backend

---
+++ server/newFile.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+
+router.get("/api", (req, res) => {
+  res.status(200).send({ message: "Hello from server boo" });
+});
+
+export default router;
