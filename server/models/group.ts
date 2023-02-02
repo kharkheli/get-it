ffbcb8c3906544b78af2e437b3c5e37355c57968
ilia kharkheli <iliakharkheli6@gmail.com>
2023-02-02 15:43:39 +0400

    add group model

---
+++ server/models/group.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+
+const GroupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide a description"],
+      trim: true,
+    },
+    members: {
+      type: [String],
+      default: [],
+    },
+    admins: {
+      type: [String],
+      default: [],
+    },
+    creator_id: {
+      type: Schema.Types.ObjectId,
+      required: [true, "Please provide a creator_id"],
+      ref: "User",
+    },
+    category: {
+      type: [String],
+      required: [true, "Please provide a category"],
+      trim: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Group", GroupSchema);
