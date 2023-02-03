7ced3a68198f0b3263f628b3eaa8283e9ee0a003
khaekheli <iliakharkheli6@gmail.com>
2023-02-03 07:19:24 +0400

    add suggestion group model

--- server/models/index.ts
+++ server/models/index.ts
@@ -2,3 +2,4 @@ export { default as User } from "./user";
 export { default as Comment } from "./comment";
 export { default as Group } from "./group";
 export { default as Post } from "./post";
+export { default as SuggestionGroup } from "./suggestionGroup";
