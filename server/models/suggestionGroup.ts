ff35e897dfbe86e0d2155b87007ce0182d2ad241
khaekheli <iliakharkheli6@gmail.com>
2023-02-03 07:18:44 +0400

    add suggestion group model

---
+++ server/models/suggestionGroup.ts
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const suggestionGroupSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a name"],
+      unique: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Please provide a description"],
+      trim: true,
+    },
+    creator_id: {
+      type: Schema.Types.ObjectId,
+      required: [true, "Please provide a creator_id"],
+      ref: "User",
+    },
+    category: {
+      type: [String],
+      required: [true, "Please provide a category"],
+      trim: true,
+    },
+    upVotes: {
+      type: [Schema.Types.ObjectId],
+      default: [],
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Group", suggestionGroupSchema);
